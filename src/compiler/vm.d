af58455a4dc59f3314f3a4d445d9704ee7b224af
Mekelaina <truemr.derpster@gmail.com>
2022-01-14 05:54:50 -0500

    started work on ram and stack implementation

--- src/compiler/vm.d
+++ src/compiler/vm.d
@@ -3,8 +3,12 @@ module compiler.vm;
 import std.stdio;
 import std.stdint;
 import std.sumtype;
+import std.conv;
+import std.array;
+import std.range : cycle, take;
 
 const AMT_REGISTERS = 10;
+const FFFF = 65_535;
 
 alias RegisterValue = SumType!(
    uint8_t,
@@ -30,6 +34,119 @@ struct Registers {
     dchar   c0, c1;
     bool     x,  y;
 
+}
+
+struct Ram {
+    
+    private ubyte[FFFF] ram;
+
+    void clearRam()
+    {
+        ram.clear();
+    }
+
+    void insertAt(ushort loc, auto value)
+    {
+        mapToRange(loc, loc, value);
+    }
+
+    void mapToRange(ushort start, ushort end, auto value)
+    in {
+        assert((end-start) % value.sizeof == 0);
+    } do {
+            ram[start..end] = (cast(ubyte*) &value)[0..value.sizeof];
+    }
+
+    ubyte[] getRange(ushort start, ushort end)
+    {
+        return ram[start..end];
+    }
+}
+
+struct Stack {
+    
+    private RegisterValue[1024] stack;
+    private ushort stackPointer = 0;
+
+    void clearStack()
+    {
+        stack.clear();
+    }
+
+    void push (RegisterValue value)
+    {
+        if(stackPointer < 1024)
+        {
+            stack[stackPointer] = value;
+            stackPointer++;
+        }
+    }
+
+    RegisterValue pop ()
+    {
+       auto rtn = stack[stackPointer];
+       stack[stackPointer] = 0;
+       stackPointer--;
+       return rtn;
+    }
+
+    RegisterValue peak()
+    {
+        return stack[stackPointer];
+    }
+
+    void drop()
+    {
+        stack[stackPointer] = 0;
+        stackPointer--;
+    }
+
+    void duplicate()
+    {
+        if(stackPointer < 1024)
+        {
+            auto top = stack[stackPointer];
+            stackPointer++;
+            stack[stackPointer] = top;
+        }
+    }
+
+    void swap()
+    {
+        auto top = stack[stackPointer];
+        stackPointer--;
+        auto second = stack[stackPointer];
+        stack[stackPointer] = top;
+        stackPointer++;
+        stack[stackPointer] = second;
+    }
+
+    ushort getSize()
+    {
+        return stackPointer;
+    }
+
+    void cycle(ushort elements, ushort cycles)
+    in {
+        assert(cycles < elements);
+    } do {
+        auto buffer = stack[(stackPointer-elements)..(stackPointer)];
+        auto temp = range.cycle(buffer).take(elements*2);
+        stack[stackPointer-elements..stackPointer] = temp[cycles..elements];
+    }
+
+    void flip()
+    {
+        RegisterValue buffer;
+        int count = 0;
+        for(int i = stackPointer; i > 0; i--)
+        {
+            buffer[count] = stack[i];
+            count++;
+        }
+        stack[0..stackPointer] = buffer[0..stackPointer];
+    }
+    
 }
 
 struct Scope {
