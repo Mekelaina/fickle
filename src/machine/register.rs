e9eadd3feb54047f6490d2be58905132fceaeb77
Dylan King <mountainboydk@gmail.com>
2022-01-25 14:28:47 -0800

    add `RegisterPool` stub

--- src/machine/register.rs
+++ src/machine/register.rs
@@ -1,15 +1,8 @@
-
+use super::Result;
+use std::ops::Index;
 
 /* two registers of each type */
 pub const REGISTER_AMT: usize = 12;
-
-
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub enum Ptr {
-    Register(u16),
-    Static(u16),
-    Memory(u16),
-}
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Register {
@@ -20,4 +13,41 @@ pub enum Register {
     Char(char),
     //String{ptr: u16, len: u16},
     Bool(bool),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Ptr {
+    Register(u16),
+    Static(u16),
+    Memory(u16),
+}
+
+const MAX_REGISTERS: usize = 1024;
+
+pub struct RegisterPool(Vec<Register>);
+
+impl Index<Ptr> for RegisterPool {
+    type Output = Register;
+
+    fn index(&self, ptr: Ptr) -> &Self::Output {
+        self.get(&ptr).unwrap()
+    }
+}
+
+impl RegisterPool {
+    fn new() -> RegisterPool {
+        RegisterPool(Vec::with_capacity(1024))
+    }
+
+    fn alloc(&self) -> Result<Ptr> {
+        todo!();
+    }
+
+    fn free(&self, ptr: Ptr) -> Result<()> {
+        todo!();
+    }
+
+    fn get(&self, ptr: &Ptr) -> Result<&Register> {
+        todo!();
+    }
 }
