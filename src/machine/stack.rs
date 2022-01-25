7b027e35e24d43f63aaecf473cacfe4b3e57a54d
OliveIsAWord <89336634+OliveIsAWord@users.noreply.github.com>
2022-01-25 14:07:11 -0800

    finished Stack methods

--- src/machine/stack.rs
+++ src/machine/stack.rs
@@ -1,9 +1,10 @@
 #![allow(dead_code)]
-use super::{Result, ProgramError, Register};
+use super::{ProgramError, Register, Result};
 
+#[derive(Debug, Clone)]
 pub struct Stack {
-    stack: [Register; 1024],
     pointer: usize,
+    stack: [Register; 1024],
 }
 
 impl Stack {
@@ -14,12 +15,16 @@ impl Stack {
         }
     }
 
-    pub fn clear_stack(&mut self) {
+    pub fn clear(&mut self) {
         self.pointer = 0;
     }
 
+    pub fn get_size(&self) -> usize {
+        self.pointer
+    }
+
     pub fn push(&mut self, value: Register) -> Result<()> {
-        if self.is_valid_pointer() {
+        if self.pointer < self.stack.len() {
             self.stack[self.pointer] = value;
             self.pointer += 1;
             Ok(())
@@ -28,11 +33,16 @@ impl Stack {
         }
     }
 
-    pub fn peek(&mut self) -> Result<Register> {
-        if self.pointer == 0 {
-            Err(ProgramError::StackUnderflow)
+    pub fn peek(&self) -> Result<Register> {
+        if let Some(ptr) = self.pointer.checked_sub(1) {
+            if ptr >= self.stack.len() {
+                panic!("Illegal stack pointer value: {}", self.pointer);
+            }
+            // NOTE: Consider using `get_unchecked` here, since the compiler doesn't
+            // know that `self.pointer` will never exceed the length of the stack
+            Ok(self.stack[ptr])
         } else {
-            Ok(self.stack[self.pointer - 1])
+            Err(ProgramError::StackUnderflow)
         }
     }
 
@@ -47,8 +57,40 @@ impl Stack {
         Ok(())
     }
 
-    fn is_valid_pointer(&self) -> bool {
-        self.stack.get(self.pointer).is_some()
+    pub fn dup(&mut self) -> Result<()> {
+        self.push(self.peek()?)?;
+        Ok(())
+    }
+
+    pub fn swap(&mut self) -> Result<()> {
+        let first = self.pop()?;
+        let second = self.pop()?;
+        self.push(first).unwrap();
+        self.push(second).unwrap();
+        Ok(())
+    }
+
+    pub fn cycle(&mut self, elements: usize, cycles: usize) -> Result<()> {
+        if let Some(bottom) = self.pointer.checked_sub(elements) {
+            let slice = &self.stack[bottom..self.pointer];
+            let rotated: Vec<_> = slice.iter().chain(slice).skip(cycles).copied().collect();
+            for (i, v) in (bottom..self.pointer).zip(rotated) {
+                self.stack[i] = v;
+            }
+            Ok(())
+        } else {
+            Err(ProgramError::StackUnderflow)
+        }
+    }
+
+    pub fn flip(&mut self) {
+        self.stack[..self.pointer].reverse();
+    }
+}
+
+impl std::fmt::Display for Stack {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:?}", &self.stack[..self.pointer])
     }
 }
 
@@ -59,6 +101,9 @@ mod tests {
     const X: Register = Register::Byte(1);
     const Y: Register = Register::Short(2);
     const Z: Register = Register::Double(3.14);
+    const A: Register = Register::ShortSigned(-42);
+    const B: Register = Register::Char('🦀');
+    const C: Register = Register::Bool(true);
 
     #[test]
     fn push_pop() -> Result<()> {
@@ -69,6 +114,17 @@ mod tests {
         assert_eq!(s.pop()?, Z);
         assert_eq!(s.pop()?, Y);
         assert_eq!(s.pop()?, X);
+        assert_eq!(s.pop(), Err(ProgramError::StackUnderflow));
+        Ok(())
+    }
+
+    #[test]
+    fn overflow() -> Result<()> {
+        let mut s = Stack::new();
+        for _ in 0..1024 {
+            s.push(X)?;
+        }
+        assert_eq!(s.push(X), Err(ProgramError::StackOverflow));
         Ok(())
     }
 
@@ -77,16 +133,121 @@ mod tests {
         let mut s = Stack::new();
         s.push(X)?;
         s.push(Y)?;
-        s.push(Z)?;
-        assert_eq!(s.peek()?, Z);
-        assert_eq!(s.peek()?, Z);
-        assert_eq!(s.pop()?, Z);
         assert_eq!(s.peek()?, Y);
         assert_eq!(s.peek()?, Y);
         assert_eq!(s.pop()?, Y);
         assert_eq!(s.peek()?, X);
         assert_eq!(s.peek()?, X);
         assert_eq!(s.pop()?, X);
+        assert_eq!(s.peek(), Err(ProgramError::StackUnderflow));
+        Ok(())
+    }
+
+    #[test]
+    fn drop() -> Result<()> {
+        let mut s = Stack::new();
+        s.push(X)?;
+        s.push(Y)?;
+        s.push(Z)?;
+        s.drop()?;
+        assert_eq!(s.pop()?, Y);
+        assert_eq!(s.pop()?, X);
+        assert_eq!(s.drop(), Err(ProgramError::StackUnderflow));
+        Ok(())
+    }
+
+    #[test]
+    fn dup() -> Result<()> {
+        let mut s = Stack::new();
+        s.push(X)?;
+        s.push(Y)?;
+        s.dup()?;
+        assert_eq!(s.pop()?, Y);
+        assert_eq!(s.pop()?, Y);
+        assert_eq!(s.pop()?, X);
+        assert_eq!(s.dup(), Err(ProgramError::StackUnderflow));
+        Ok(())
+    }
+
+    #[test]
+    fn swap() -> Result<()> {
+        let mut s = Stack::new();
+        s.push(X)?;
+        s.push(Y)?;
+        s.swap()?;
+        assert_eq!(s.pop()?, X);
+        assert_eq!(s.pop()?, Y);
+        assert_eq!(s.swap(), Err(ProgramError::StackUnderflow));
+        s.push(Z)?;
+        assert_eq!(s.swap(), Err(ProgramError::StackUnderflow));
+        Ok(())
+    }
+
+    #[test]
+    fn cycle_3() -> Result<()> {
+        let mut s = Stack::new();
+        s.push(A)?;
+        s.push(X)?;
+        s.push(Y)?;
+        s.push(Z)?;
+        let mut s1 = s.clone();
+        s1.cycle(3, 1)?;
+        assert_eq!(s1.pop()?, X);
+        assert_eq!(s1.pop()?, Z);
+        assert_eq!(s1.pop()?, Y);
+        assert_eq!(s1.pop()?, A);
+        assert_eq!(s1.pop(), Err(ProgramError::StackUnderflow));
+        let mut s2 = s.clone();
+        s2.cycle(3, 2)?;
+        assert_eq!(s2.pop()?, Y);
+        assert_eq!(s2.pop()?, X);
+        assert_eq!(s2.pop()?, Z);
+        assert_eq!(s2.pop()?, A);
+        assert_eq!(s2.pop(), Err(ProgramError::StackUnderflow));
+        Ok(())
+    }
+
+    #[test]
+    fn cycle_5() -> Result<()> {
+        let mut s = Stack::new();
+        s.push(A)?;
+        s.push(B)?;
+        s.push(C)?;
+        s.push(X)?;
+        s.push(Y)?;
+        s.push(Z)?;
+        s.cycle(5, 2)?;
+        assert_eq!(s.pop()?, C);
+        assert_eq!(s.pop()?, B);
+        assert_eq!(s.pop()?, Z);
+        assert_eq!(s.pop()?, Y);
+        assert_eq!(s.pop()?, X);
+        assert_eq!(s.pop()?, A);
+        assert_eq!(s.pop(), Err(ProgramError::StackUnderflow));
+        Ok(())
+    }
+
+    #[test]
+    fn flip() -> Result<()> {
+        let mut s = Stack::new();
+        s.push(X)?;
+        s.push(Y)?;
+        s.push(Z)?;
+        s.flip();
+        assert_eq!(s.pop()?, X);
+        assert_eq!(s.pop()?, Y);
+        assert_eq!(s.pop()?, Z);
+        assert_eq!(s.swap(), Err(ProgramError::StackUnderflow));
+        Ok(())
+    }
+
+    #[test]
+    fn to_string() -> Result<()> {
+        let mut s = Stack::new();
+        s.push(X)?;
+        s.push(Y)?;
+        s.push(Z)?;
+        assert_eq!(s.to_string(), "[Byte(1), Short(2), Double(3.14)]");
         Ok(())
     }
 }
