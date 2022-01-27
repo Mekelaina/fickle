91270e16ac90c9ea2d0d17cb86a415e098c88c89
Dylan King <mountainboydk@gmail.com>
2022-01-27 11:17:43 -0800

    Begin implementing scope operations

--- src/machine/scope.rs
+++ src/machine/scope.rs
@@ -1,10 +1,9 @@
 use super::pointer::Ptr;
+use super::register::{Reg, Register, RegisterPool, REGISTER_AMT};
 use super::Result;
-use super::register::{Register, Reg, RegisterPool, REGISTER_AMT};
 
-use std::ops::{Index, IndexMut};
 use std::fmt;
-
+use std::ops::{Index, IndexMut};
 
 pub struct Scope<'a> {
     ptrs: [Ptr; REGISTER_AMT],
@@ -15,46 +14,69 @@ impl fmt::Debug for Scope<'_> {
     fn fmt(&self, _f: &mut fmt::Formatter<'_>) -> fmt::Result {
         // TODO: Debug needs to be implemented manually here because we don't
         // want printing a scope to also print the whole contents of the
-        // RegisterPool it's allocated in. 
+        // RegisterPool it's allocated in.
         todo!();
-    }    
+    }
 }
 
 impl Index<Reg> for Scope<'_> {
-    type Output = Ptr;
-    
+    type Output = Register;
+
     fn index(&self, r: Reg) -> &Self::Output {
-        &self.ptrs[r as usize]
+        self.ator.get(self.ptrs[r as usize]).unwrap()
     }
 }
 
 impl IndexMut<Reg> for Scope<'_> {
     fn index_mut(&mut self, r: Reg) -> &mut Self::Output {
-        &mut self.ptrs[r as usize]
+        self.ator.get_mut(self.ptrs[r as usize]).unwrap()
     }
 }
 
 impl Scope<'_> {
     fn new(ator: &mut RegisterPool) -> Result<Scope> {
         Ok(Scope {
-            ptrs: [ator.alloc_with(Register::Byte(0u8))?,
-               ator.alloc_with(Register::Byte(0u8))?,
-               ator.alloc_with(Register::Word(0i16))?,
-               ator.alloc_with(Register::Word(0i16))?,
-               ator.alloc_with(Register::Pointer(Ptr::Null))?,
-               ator.alloc_with(Register::Pointer(Ptr::Null))?,
-               ator.alloc_with(Register::Float(0f64))?,
-               ator.alloc_with(Register::Float(0f64))?,
-               ator.alloc_with(Register::Char('\0'))?,
-               ator.alloc_with(Register::Char('\0'))?,
-               // ator.alloc_with(Register::Str())?,
-               // ator.alloc_with(Register::Str())?,
-               ator.alloc_with(Register::Bool(false))?,
-               ator.alloc_with(Register::Bool(false))?,
+            ptrs: [
+                ator.alloc_with(Register::Byte(0u8))?,
+                ator.alloc_with(Register::Byte(0u8))?,
+                ator.alloc_with(Register::Word(0i16))?,
+                ator.alloc_with(Register::Word(0i16))?,
+                ator.alloc_with(Register::Pointer(Ptr::Null))?,
+                ator.alloc_with(Register::Pointer(Ptr::Null))?,
+                ator.alloc_with(Register::Float(0f64))?,
+                ator.alloc_with(Register::Float(0f64))?,
+                ator.alloc_with(Register::Char('\0'))?,
+                ator.alloc_with(Register::Char('\0'))?,
+                // ator.alloc_with(Register::Str())?,
+                // ator.alloc_with(Register::Str())?,
+                ator.alloc_with(Register::Bool(false))?,
+                ator.alloc_with(Register::Bool(false))?,
             ],
-            ator: ator,
+            ator,
         })
     }
 
-    fn mov() { todo!(); }
+    fn mov(&mut self, into: Reg, what: Register) {
+        /* TODO: check that what and into are the same type */
+        self[into] = what;
+    }
+
+    fn bnd(&mut self, to: Reg, what: Ptr) {
+        /* TODO: check that what and to are the same type */
+        self.ptrs[to as usize] = what;
+    }
 }
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn two_scopes() -> Result<()> {
+        todo!(); 
+    }
+
+
+
+
+}
+
