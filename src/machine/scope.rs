872c08ada0be1c313760637623d95c466a7c81ac
Dylan King <mountainboydk@gmail.com>
2022-01-27 00:11:16 -0800

    Start to implement scopes

--- src/machine/scope.rs
+++ src/machine/scope.rs
@@ -1,10 +1,60 @@
-use super::pointer::{Ptr};
-use super::register::REGISTER_AMT;
-#[derive(Debug)]
-pub struct Scope([Ptr; REGISTER_AMT]);
+use super::pointer::Ptr;
+use super::Result;
+use super::register::{Register, Reg, RegisterPool, REGISTER_AMT};
 
-impl Scope {
-    fn new() -> Scope {
+use std::ops::{Index, IndexMut};
+use std::fmt;
+
+
+pub struct Scope<'a> {
+    ptrs: [Ptr; REGISTER_AMT],
+    ator: &'a mut RegisterPool,
+}
+
+impl fmt::Debug for Scope<'_> {
+    fn fmt(&self, _f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // TODO: Debug needs to be implemented manually here because we don't
+        // want printing a scope to also print the whole contents of the
+        // RegisterPool it's allocated in. 
         todo!();
+    }    
+}
+
+impl Index<Reg> for Scope<'_> {
+    type Output = Ptr;
+    
+    fn index(&self, r: Reg) -> &Self::Output {
+        &self.ptrs[r as usize]
     }
+}
+
+impl IndexMut<Reg> for Scope<'_> {
+    fn index_mut(&mut self, r: Reg) -> &mut Self::Output {
+        &mut self.ptrs[r as usize]
+    }
+}
+
+impl Scope<'_> {
+    fn new(ator: &mut RegisterPool) -> Result<Scope> {
+        Ok(Scope {
+            ptrs: [ator.alloc_with(Register::Byte(0u8))?,
+               ator.alloc_with(Register::Byte(0u8))?,
+               ator.alloc_with(Register::Word(0i16))?,
+               ator.alloc_with(Register::Word(0i16))?,
+               ator.alloc_with(Register::Pointer(Ptr::Null))?,
+               ator.alloc_with(Register::Pointer(Ptr::Null))?,
+               ator.alloc_with(Register::Float(0f64))?,
+               ator.alloc_with(Register::Float(0f64))?,
+               ator.alloc_with(Register::Char('\0'))?,
+               ator.alloc_with(Register::Char('\0'))?,
+               // ator.alloc_with(Register::Str())?,
+               // ator.alloc_with(Register::Str())?,
+               ator.alloc_with(Register::Bool(false))?,
+               ator.alloc_with(Register::Bool(false))?,
+            ],
+            ator: ator,
+        })
+    }
+
+    fn mov() { todo!(); }
 }
